3c05defa1faab3bd55e06a161944b99229b3a670
Natsu-Efrain <67449084+Natsu-Efrain@users.noreply.github.com>
2021-10-26 21:44:49 -0500

    Delete proyecto-consolecode directory

--- proyecto-consolecode/src/main/java/com/consolecode/repository/IPaisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.consolecode.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.consolecode.model.Pais;
-
-public interface IPaisRepository extends JpaRepository<Pais, Integer>{
-
-}
